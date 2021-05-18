37a9dece1472446804b8323b533b6ae40aea48c3
AlekseyGladyshko <43505635+AlekseyGladyshko@users.noreply.github.com>
2021-05-18 20:23:18 +0300

    Delete old folder

--- PMKR/task2.R
+++
@@ -1,6 +0,0 @@
-library(XML)
-xmlUrl = "http://d396qusza40orc.cloudfront.net/getdata%2Fdata%2Frestaurants.xml"
- 
-download.file(xmlUrl, "data.xml", "auto", TRUE)
-data <- xmlParse("data.xml")
-sum(xpathSApply(data, "//zipcode", xmlValue) == 21231)
